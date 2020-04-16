0f2a389d15b4be9a74d9c2838a640b710c626da9
wwt-git-foundations <63331032+wwt-git-foundations@users.noreply.github.com>
2020-04-16 16:24:14 -0700

    Removed extra commands

--- my_script.py
+++ my_script.py
@@ -2,7 +2,3 @@
 # This script says hello
 name = input("What is your name? ")
 print(f"It is nice to meet you, {name}")
-#!/usr/bin/python
-# This script says hello
-name = input("What is your name? ")
-print(f"It is nice to meet you, {name})
